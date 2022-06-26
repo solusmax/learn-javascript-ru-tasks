e42029c881e9d9a4323e53e5eea3366fcce34e9b
solusmax <solusmaxim@gmail.com>
2022-06-26 19:13:27 +0300

    Добавил решение к 2-17, задача 1

---
+++ part-1/2-17/task-1/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const ask = (question, yes, no) => confirm(question) ? yes() : no();
+
+ask(
+  'Вы согласны?',
+  () => alert('Вы согласились.'),
+  () => alert('Вы отменили выполнение.')
+);
