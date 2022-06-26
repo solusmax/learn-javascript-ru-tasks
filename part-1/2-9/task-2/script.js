8d44ebec2509d53a8b9d000005cc8944c7d6eff3
solusmax <solusmaxim@gmail.com>
2022-06-26 14:50:03 +0300

    Добавил решение к 2-9, задача 2

---
+++ part-1/2-9/task-2/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let userAnswer = prompt('Какое «официальное» название JavaScript?', '');
+
+if (userAnswer.toLowerCase() === 'ecmascript') {
+	alert('Верно!');
+} else {
+	alert('Не знаете? ECMAScript!')
+};
