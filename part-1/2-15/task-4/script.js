fa5ed1b0549ad32c38a83501f05bc346fbb87eda
solusmax <solusmaxim@gmail.com>
2022-06-26 18:05:50 +0300

    Добавил решение к 2-15, задача 4

---
+++ part-1/2-15/task-4/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt('x:', '');
+let n = prompt('n:', '');
+
+alert(pow(x, n));
