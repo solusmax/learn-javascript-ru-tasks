2acb33ec639a36fcd3c5bbba1d6909af35a80483
solusmax <solusmaxim@gmail.com>
2022-06-26 16:05:32 +0300

    Добавил решение к 2-11, задача 9

---
+++ part-1/2-11/task-9/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let userLogin = prompt("Кто там?", '');
+
+if (userLogin === 'Админ') {
+  let userPassword = prompt('Пароль?', '');
+
+  if (userPassword === 'Я главный') {
+    alert('Здравствуйте!');
+  } else if (userPassword === '' || userPassword === null) {
+    alert('Отменено' );
+  } else {
+    alert('Неверный пароль');
+  }
+} else if (userLogin === '' || userLogin === null) {
+  alert( 'Отменено' );
+} else {
+  alert( "Я вас не знаю" );
+}
