b051cea6825f96fdad0100119af8b4a5ee41e76e
solusmax <solusmaxim@gmail.com>
2022-06-26 15:59:53 +0300

    Добавил решение к 2-11, задача 7

---
+++ part-1/2-11/task-7/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+let a = 27;
+let isBetween;
+
+if (!(a >= 14 && a <= 90)) {
+	isBetween = true;
+} else {
+	isBetween = false;
+}
+
+if (a < 14 || a > 90) {
+	isBetween = true;
+} else {
+	isBetween = false;
+}
