8fe71d609d1657b4f547fd2a49cd6ce665866387
solusmax <solusmaxim@gmail.com>
2022-06-27 12:50:51 +0300

    Добавил решение к 4-1, задача 5

---
+++ part-1/4-1/task-5/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+const doubleEveryNumericProperty = (obj) => {
+  for (const [key, value] of Object.entries(obj)) {
+    if (typeof(value) === 'number') {
+      obj[key] = value * 2;
+    }
+  }
+};
+
