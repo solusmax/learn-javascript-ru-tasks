65590f0013a6c3a0fe1140712f0b9ef984d2c5c2
solusmax <solusmaxim@gmail.com>
2022-06-27 13:00:41 +0300

    Добавил решение к 4-4, задача 5

---
+++ part-1/4-4/task-5/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    alert(this.step);
+    return this;
+  }
+};
