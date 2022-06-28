6ccc42d0c41fc7adefc86e44f7172813b74342f6
solusmax <solusmaxim@gmail.com>
2022-06-28 11:54:20 +0300

    Добавил решение к 5-5, задача 11

---
+++ part-1/5-5/task-11/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const vasya = {
+  name: "Вася",
+  age: 25
+};
+
+const petya = {
+  name: "Петя",
+  age: 30
+};
+
+const masha = {
+  name: "Маша",
+  age: 29
+};
+
+const arr = [vasya, petya, masha];
+
+const getAverageAge = (users) => users.reduce((acc, el) => acc + el.age, 0) / arr.length;
