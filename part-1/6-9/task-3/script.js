ebdf04e2af37a6f4240ea88e54bbb5f216ef5bf7
solusmax <solusmaxim@gmail.com>
2022-06-28 22:55:48 +0300

    Добавил решение к 6-9, задача 3

---
+++ part-1/6-9/task-3/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function debounce(func, ms) {
+  let isCooldown = false;
+
+  return function() {
+    if (isCooldown) return;
+
+    func.apply(this, arguments);
+
+    isCooldown = true;
+
+    setTimeout(() => isCooldown = false, ms);
+  };
+}
