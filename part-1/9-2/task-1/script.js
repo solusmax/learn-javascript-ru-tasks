8cdd1a32a8c23593bb9ad381cbeaeed34663b36c
solusmax <solusmaxim@gmail.com>
2022-06-28 08:24:09 +0300

    Добавил решение к 9-2, задача 1

---
+++ part-1/9-2/task-1/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class Rabbit extends Animal {
+  constructor(name) {
+    super(name);
+    this.created = Date.now();
+  }
+}
+
+const rabbit = new Rabbit("Белый кролик");
+alert(rabbit.name);
