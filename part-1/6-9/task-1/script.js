d467e83214aef8e6adad6aee429d10eb7f67b531
solusmax <solusmaxim@gmail.com>
2022-06-28 22:35:48 +0300

    Добавил решение к 6-9, задача 1

---
+++ part-1/6-9/task-1/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function spy(func) {
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.apply(this, arguments);
+  }
+
+  wrapper.calls = [];
+
+  return wrapper;
+}
